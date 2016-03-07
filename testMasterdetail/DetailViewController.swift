1eb0a49e69daa1f3f9262dde6d5a85877afdb630
shiwwgis <chinagis001@126.com>
2016-03-07 22:47:06 +0800

    detailview中增加了多个rightbuttonItem

--- testMasterdetail/DetailViewController.swift
+++ testMasterdetail/DetailViewController.swift
@@ -36,6 +36,24 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.configureView()
+        //1.右边第一个按钮
+        let composeButton = UIBarButtonItem(barButtonSystemItem:.Compose, target: self, action: nil)
+        //2.second
+        let replyButton = UIBarButtonItem(barButtonSystemItem:.Reply, target: self, action: nil)
+        let trashButton = UIBarButtonItem(barButtonSystemItem:.Trash, target: self, action: nil)
+        
+        let organizeButton = UIBarButtonItem(barButtonSystemItem:.Organize, target: self, action: nil)
+        //如果是自定义图片,必须得有imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal),否则是个纯色图片
+        let moreButton = UIBarButtonItem(image: UIImage(named: "master3")!.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal), style: UIBarButtonItemStyle.Plain, target: self,action: nil)
+
+
+        
+        let rightItems=[composeButton,replyButton,trashButton,organizeButton,moreButton];
+
+
+        self.navigationItem.rightBarButtonItems = rightItems
+
+        
     }
 
     override func didReceiveMemoryWarning() {
